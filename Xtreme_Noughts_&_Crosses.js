712fb356bb03894dc559bd7b1d2027dd8d4d8322
MerlinTheWizard1313 <merlin.britt@icloud.com>
2025-05-15 17:07:57 +0100

    XNC - XN_C class (creates a full board)

--- Xtreme_Noughts_&_Crosses.js
+++ Xtreme_Noughts_&_Crosses.js
@@ -17,11 +17,12 @@ var currentTurn = "";
 class N_C {
     constructor(game, number){
         this.box = game;
-        this.boardNumber = number; //Set the board number from 1-9 from input
+        this.boardNumber = number; //Set the board number from 1-9 from input unless it is the main board
         this.boardNumberClass = "game-board-" + this.boardNumber.toString(); //Set name of board class
         this.win_status = false; //Has the current board won?
+        this.boardActive = false; //Is the current board the active board?
         this.winner = ""; //Winner of the current board?
-        this.genBoard();
+        this.genBoard(number); //Generate the board grid unless number = 0
         this.xArr = []; //Current Player moves
         this.oArr = []; //Current Ai moves
         this.emptyArr = [1,2,3,4,5,6,7,8,9]; //Moves Remaining
@@ -29,23 +30,17 @@ class N_C {
     }
     
     //Turn toggle to flip between player and ai turn
-    setTurn(){
-        currentTurn = (currentTurn == "xTurn" ? "oTurn" : "xTurn");
-    }
-
-    //Initial randomiser of turn order once the game begins
-    turnRandom(){
-        const random = Math.random();
-        return random >= 0.5 ? true : false;
+    toggleTurn(){
+        if (boardActive){
+            currentTurn = (currentTurn == "xTurn" ? "oTurn" : "xTurn");
+            this.boardActive = false;
+        }
     }
 
     //Upon the call of the XN_C class, this will intitialise the game board and game state
     //so the game can begin
     startBoard(){
-        currentTurn = this.turnRandom();
-        this.setTurn();
         this.win_status = false;
-        console.log(currentTurn);
     }
 
     //After each move, this is called to check if the current move has caused a win on the board
@@ -83,17 +78,18 @@ class N_C {
     }
 
     //Create the visual grid in html with event listeners to the grid spaces
-    genBoard(){
-        const board = document.createElement("div");
-        board.classList.add("game-board", this.boardNumberClass);
-        for (let i = 0; i < 9; i++){
-            const div = document.createElement("div");
-            div.setAttribute("data-id", i + 1);
-            board.appendChild(div);
+    genBoard(number){
+        if (number != 0){
+            const board = document.createElement("div");
+            board.classList.add("game-board", this.boardNumberClass);
+            for (let i = 0; i < 9; i++){
+                const div = document.createElement("div");
+                div.setAttribute("data-id", i + 1);
+                board.appendChild(div);
+            }
+            board.addEventListener("click", this.playerTurn.bind(this));
+            this.box.appendChild(board);
         }
-        board.addEventListener("click", this.playerTurn.bind(this));
-        this.box.appendChild(board);
-
         /*const winnerOutput = document.createElement("div"); will keep this here for later when a position is decided
 		winnerOutput.classList.add("winner-output");
 		this.winnerOutput = this.container.appendChild(winnerOutput);*/
@@ -124,27 +120,67 @@ class N_C {
     }
 }
 
-//Find the div in project2.html which will house the game board
-const box = document.querySelector(".game-box-1");
-const box2 = document.querySelector(".game-box-2");
-const box3 = document.querySelector(".game-box-3");
-const box4 = document.querySelector(".game-box-4");
-const box5 = document.querySelector(".game-box-5");
-const box6 = document.querySelector(".game-box-6");
-const box7 = document.querySelector(".game-box-7");
-const box8 = document.querySelector(".game-box-8");
-const box9 = document.querySelector(".game-box-9");
+class XN_C extends N_C{
+    constructor(box){
+        super(box, 0);
+        currentTurn = this.turnRandom();
+
+    }
+
+    genBoards(){
+        const box1 = document.querySelector(".game-box-1");
+        const box2 = document.querySelector(".game-box-2");
+        const box3 = document.querySelector(".game-box-3");
+        const box4 = document.querySelector(".game-box-4");
+        const box5 = document.querySelector(".game-box-5");
+        const box6 = document.querySelector(".game-box-6");
+        const box7 = document.querySelector(".game-box-7");
+        const box8 = document.querySelector(".game-box-8");
+        const box9 = document.querySelector(".game-box-9");
+        const game1 = new N_C(box1,1);
+        const game2 = new N_C(box2,2);
+        const game3 = new N_C(box3,3);
+        const game4 = new N_C(box4,4);
+        const game5 = new N_C(box5,5);
+        const game6 = new N_C(box6,6);
+        const game7 = new N_C(box7,7);
+        const game8 = new N_C(box8,8);
+        const game9 = new N_C(box9,9); 
+    }
+    //Upon the call of the XN_C class, this will intitialise the game board and game state
+    //so the game can begin
+    startBoard(){
+        this.win_status = false;
+    }
+
+    //Initial randomiser of turn order once the game begins
+    turnRandom(){
+        const random = Math.random();
+        return random >= 0.5 ? "xTurn" : "oTurn";
+    }
+
+    //will be overidden
+    winRowCheck(){
+        if(this.win_status == false){
+            let row, arrayToCheck;
+            arrayToCheck = (currentTurn == "xTurn" ? this.xArr : this.oArr);
+            if (arrayToCheck.length >= 3){
+                winArr.forEach((winRow)=>{
+                    if (winRow.every(x => this.xArr.includes(x))) {
+                        this.winner = currentTurn;
+                        //row = winRow.slice();
+                        this.win_status = true;
+                    }
+                })
+            }
+        }
+    }
+
+}
+
 //Create a new Xtreme Noughts and Crosses board class 
 //Which will call the creation of 9 other mini boards through the N_C class
 //const game = new N_C(box,1);
 
-//delete this
-const game = new N_C(box,1);
-const game2 = new N_C(box2,2);
-const game3 = new N_C(box3,3);
-const game4 = new N_C(box4,4);
-const game5 = new N_C(box5,5);
-const game6 = new N_C(box6,6);
-const game7 = new N_C(box7,7);
-const game8 = new N_C(box8,8);
-const game9 = new N_C(box9,9);
+const boardbox = document.querySelector("#large-game-box");
+const game0 = new XN_C(boardbox);
