84bf9bc0cebb719dddadf94d6d9051e1b56818e1
MerlinTheWizard1313 <merlin.britt@icloud.com>
2025-05-16 18:31:54 +0100

    XNC n_c class - T5.1

--- Xtreme_Noughts_&_Crosses.js
+++ Xtreme_Noughts_&_Crosses.js
@@ -102,7 +102,7 @@ class N_C {
         this.toggleTurn();
         if (boardStore == 10){
             document.querySelector(".game-prompt").innerHTML = "";
-            for(let i = 0; i > 9; i++){
+            for(let i = 0; i < 9; i++){
                 if (i != 4){
                     gameBoardArr[i].boardActive = false;
                     gameBoardArr[i].box.classList.remove("current-board-active");
@@ -193,7 +193,7 @@ class XN_C extends N_C{
     }
     
     resetBoards(){
-        for(let i = 0; i > 9; i++){
+        for(let i = 0; i < 9; i++){
             gameBoardArr[i].resetBoard();
         }
     }
@@ -202,11 +202,11 @@ class XN_C extends N_C{
             if (currentTurn == "xTurn"){
                 boardStore = 10;
                 document.querySelector(".game-prompt").innerHTML = "Choose a move on any of the other boards!";
-                for(let j = 0; j > 9; j++){
-                    if (j != 4){
-                        gameBoardArr[j].boardActive = true;
-                        gameBoardArr[j].box.classList.add("current-board-active");
-                        gameBoardArr[j].startBoard();
+                for(let i = 0; i < 9; i++){
+                    if (i != 4){
+                        gameBoardArr[i].boardActive = true;
+                        gameBoardArr[i].box.classList.add("current-board-active");
+                        gameBoardArr[i].startBoard();
                     }
                 }
             } else {
