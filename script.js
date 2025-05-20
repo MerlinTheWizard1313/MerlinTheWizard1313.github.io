8bdc46c9d227099ecc243c77c698eccb129c0ffc
MerlinTheWizard1313 <merlin.britt@icloud.com>
2025-05-20 14:01:23 +0100

    Bnp Slideshow - T2 - Added image folder and extended html elements and function to fit

--- script.js
+++ script.js
@@ -25,8 +25,12 @@ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("bnp-slides");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
