8a45497cac3c71c0a104d61c168a48c0f4288e7c
MerlinTheWizard1313 <merlin.britt@icloud.com>
2025-08-29 11:59:20 +0100

    Side Project P2 - T2 - Stage 2 Beginning

---
+++ arg-script.js
@@ -0,0 +1,30 @@
+function handlePartnerAnswers(event){
+    event.preventDefault();
+    let partner1Answer = document.getElementById("partner1");
+    let partner2Answer = document.getElementById("partner1");
+    let partnerAnswerValidated = partnerValidation(partner1Answer,partner2Answer);
+    if (partnerAnswerValidated){
+        setPartnerEquation();
+    }
+}
+
+function partnerValidation(partner1,partner2) {
+    partner1Check = partner1.toLowerCase();
+    partner2Check = partner2.toLowerCase();
+    if (partner1Check == "joseph") {
+        if (partner2Check == "perceval"){
+            return true;
+        }
+    } else if (partner1Check == "perceval"){
+        if (partner2Check == "joseph"){
+            return true;
+        }
+    } else {
+        return false;
+    }
+}
+
+function setPartnerEquation(){
+    let partnerEquation = document.getElementById("partnerEquation");
+    partnerEquation.innerHTML = "a";
+}
