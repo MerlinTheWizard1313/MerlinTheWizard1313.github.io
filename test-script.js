eca84083b4be79484f2630d314364497839fbe59
MerlinTheWizard1313 <merlin.britt@icloud.com>
2025-09-27 12:53:50 +0100

    Side Project P3 - T1.0 - Initial testing of generating a grid

---
+++ test-script.js
@@ -0,0 +1,25 @@
+class MazeSquare {
+    constructor(box){
+        this.gridSquare = box;
+
+    }
+}
+
+class Maze extends MazeSquare{
+    constructor(grid){
+        this.mainGrid = grid;
+    }
+
+    genGrid(){
+        for(const child of mainGrid){
+            if (child.dataset.id != "A" || child.dataset.id != "B"){
+                child.style.color = "red";
+            } else {
+                child.style.color = "blue";
+            }
+        }
+    }
+}
+
+const gridBox = document.querySelector("#mazeBox");
+const gridTest = new Maze(gridBox);
