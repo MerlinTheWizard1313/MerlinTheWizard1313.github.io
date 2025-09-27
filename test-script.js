9ee20df14e5d34c2f6794fb7a7a40e09c3ca68ee
MerlinTheWizard1313 <merlin.britt@icloud.com>
2025-09-27 14:30:46 +0100

    Side Project P3 - T1.101 - Add check so a grid letter does not change and debug logs

--- test-script.js
+++ test-script.js
@@ -6,25 +6,32 @@ class MazeSquare {
         this.wallArray = ["North", "East", "South", "West"];
         this.lightLevelNumber = 0;
         this.lightColour = "black";
+        this.gridContent = "";
         this.genGridContent();
     }
 
     genGridContent(rowLetter,columnNumber){
-        var randomNumber = Math.random();
-        switch(randomNumber){
-            case randomNumber <= 0.25:
-                this.gridContent = "Empty"
-                break;
-            case randomNumber <= 0.5:
-                this.gridContent = "Unlit Torch"
-                break;
-            case randomNumber <= 0.75:
-                this.gridContent = "Enemy"
-                break;
-            case randomNumber <= 1:
-                this.gridContent = "NPC"
-                break;           
+        if(columnNumber != 0){
+            var randomNumber = Math.random();
+            switch(randomNumber){
+                case randomNumber <= 0.25:
+                    this.gridContent = "Empty"
+                    break;
+                case randomNumber <= 0.5:
+                    this.gridContent = "Unlit Torch"
+                    break;
+                case randomNumber <= 0.75:
+                    this.gridContent = "Enemy"
+                    break;
+                case randomNumber <= 1:
+                    this.gridContent = "NPC"
+                    break;     
+            }      
+        } else {
+            this.gridContent = rowLetter;
         }
+        console.log(this.gridSquare);
+        console.log(this.gridSquare.innerText);
         this.gridSquare.innerText = this.gridContent;
     }
 }
@@ -39,9 +46,11 @@ class Maze{
         for(var child of this.mainGrid.children){
             if (child.dataset.id == "A" || child.dataset.id == "B"){
                 child.style.color = "blue";
+                console.log(child);
                 child = new MazeSquare(child, child.dataset.id, 0)
             } else {
                 child.style.color = "red";
+                console.log(child);
                 child = new MazeSquare(child, "test", child.dataset.id)
             }
         }
