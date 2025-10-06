e7029e40141820701bf65536e165bbf62485da09
MerlinTheWizard1313 <merlin.britt@icloud.com>
2025-10-06 23:45:53 +0100

    Side Project P3 - T1.270 - Set up test2 page with new scripts, testing the fetch function to see if it gives desired effect, added wall images for the puzzles

---
+++ test-2-script.js
@@ -0,0 +1,60 @@
+function changeWall(wallChoice){
+    let holder = document.querySelector(".wall-holder");
+    holder.removeChild(holder.firstElementChild);
+    switch(wallChoice){
+        case 0:
+            holder.innerHTML += `<img src="images/SecretAssets/Wall1.png" style="width:100%"></img>`
+            break;
+        case 1:
+            holder.innerHTML += `<img src="images/SecretAssets/Wall2.png" style="width:100%"></img>`
+            break;
+        case 2:
+            holder.innerHTML += `<img src="images/SecretAssets/Wall3.png" style="width:100%"></img>`
+            break;
+        case 3:
+            holder.innerHTML += `<img src="images/SecretAssets/Wall4.png" style="width:100%"></img>`
+            break;
+        default:
+            return "error";
+    }
+}
+
+function handleCodeAnswer(event){
+    event.preventDefault();
+    let code = document.getElementById("code").value;
+    let codeValidated = codeValidation(code);
+    setReward(codeValidated);
+}
+
+function codeValidation(code) {
+    let codeCheck = 0;
+    fetch('images/SecretAssets/Code.txt')
+        .then(response => response.text())
+        .then(text => {
+            console.log(text);
+            codeCheck = Integer.parseInt(text)
+        })
+    if (code == codeCheck) {
+        return true;
+    } else {
+        console.log(codeCheck);
+        return false;
+    }
+}
+
+function setReward(check){
+    let errorBox = document.getElementById("error-code");
+    if (check){
+        window.location.href = "test-4.html";
+    } else {
+        errorBox.innerText = "Incorrect Code. Try Again";
+    }
+}
+
+function fetchTest(){
+    fetch('images/SecretAssets/Code.txt')
+        .then(response => response.text())
+        .then(text => console.log(text))
+}
+
+fetchTest();
